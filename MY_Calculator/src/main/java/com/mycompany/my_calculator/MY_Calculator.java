18a6f3c2f20702c63d5a36dd36c61dacc78d6b87
Victor <147107766+Crosby01@users.noreply.github.com>
2024-11-29 12:05:37 +0100

    Add files via upload

---
+++ MY_Calculator/src/main/java/com/mycompany/my_calculator/MY_Calculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.my_calculator;
+
+/**
+ *
+ * @author Victor
+ */
+public class MY_Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
